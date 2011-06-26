e5bf32cd81e03b852b93ddc3961cee0df2b0a213
Ripan <er.ripankumar@gmail.com>
2011-06-26 20:53:19 +0100

    is_verified? added

--- lib/acts_as_verified.rb
+++ lib/acts_as_verified.rb
@@ -8,15 +8,16 @@ module ActiveRecord #:nodoc:
       module ClassMethods
         def acts_as_verified
           has_one :state, :as => :verifyable , :dependent => :destroy
-          before_save :check_state_change
+          before_save :check_state_change 
           include ActiveRecord::Acts::Verified::InstanceMethods
         end
       end
       module InstanceMethods
-
+        UNVERIFIED = "unverified"   
+        VERIFIED = "verified"
         def verification_status
           if self.state.blank?
-            self.build_state(:name => 'unverified')
+            self.build_state(:name => UNVERIFIED)
             self.state.save
           end
           return self.state.name
@@ -27,15 +28,19 @@ module ActiveRecord #:nodoc:
         end
 
         def set_as_verified
-          self.verification_status = "verified"
+          self.verification_status = VERIFIED
         end
 
         def set_as_unverified
-          self.verification_status = "unverified"
+          self.verification_status = UNVERIFIED
         end
 
         def check_state_change
           self.set_as_unverified if self.changed?
+        end 
+
+        def is_verified?
+          self.state.name ==  VERIFIED
         end
 
       end
